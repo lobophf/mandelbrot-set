cf56ddcb2542a274db6aceafba805c33da609e59
Pedro Henrique Fernandes Lobo <lobophf@gmail.com>
2020-02-07 20:52:38 -0300

    I'm uploading the task of lesson S7L35

--- main.cpp
+++ main.cpp
@@ -1,26 +1,37 @@
 #include<iostream>
+#include<algorithm>
+#include<vector>
 
-void test(){
-	std::cout << "hello" << std::endl;
-
+bool match(std::string test){
+	
+	return test.size() == 3;
 }
 
-void test2(int n){
-	std::cout << "value: " << n << std::endl;
+int countString(std::vector<std::string> &texts, bool (*match)(std::string test) ){
+	int tally = 0;
+	for(int i = 0; i < texts.size(); i++){
+		if(match(texts[i])){
+			tally++;
+		}
+	}
+	return tally;
 }
 
 
 int main(){
+
+	std::vector<std::string> texts;
+	texts.push_back("one");
+	texts.push_back("two");
+	texts.push_back("three");
+	texts.push_back("four");
+	texts.push_back("two");
+	texts.push_back("three");
 	
+	std::cout << match("one") << std::endl;
+	
+	std::cout << std::count_if(texts.begin(), texts.end(), match) << std::endl;
 
-	test();
-	void (*pTest)() = test;
-
-	pTest();
-
-	test2(2);
-	void (*pTest2)(int) = test2;
-
-	pTest2(8);
-
+	std::cout << countString(texts, match) << std::endl;
+	return 0;
 }
