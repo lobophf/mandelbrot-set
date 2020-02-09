e61be40ae776c7cc59f8be6406e67a818c491559
Pedro Henrique Fernandes Lobo <lobophf@gmail.com>
2020-02-09 01:21:55 -0300

    I'm uploading the task of lecture S8L41

--- main.cpp
+++ main.cpp
@@ -1,26 +1,29 @@
 #include<iostream>
-#include<typeinfo>
-
-class myClass{
-public:
-	myClass(){}
-	~myClass(){}
-};
+#include<vector>
 
 int main(){
 	
-	double i;
+	auto texts = {"one", "two", "three"};
+	for(auto i: texts){
+		std::cout << i << std::endl;
+	}
+		
+	std::vector<int> numbers;
+	numbers.push_back(3);
+	numbers.push_back(5);
+	numbers.push_back(6);
+	numbers.push_back(11);
+
+	for(auto i: numbers){
+		std::cout << i << std::endl;
+	}
+
+	std::string myString = "hello";
 	
-	std::cout << typeid(i).name() << std::endl;
-
-	std::string myString;
-	std::cout << typeid(myString).name() << std::endl;
-
-	char myChar;
-	std::cout << typeid(myChar).name() << std::endl;
-
-	myClass p;
-	std::cout << typeid(p).name() << std::endl;
+	std::cout << std::endl;
+	for(auto c: myString){
+		std::cout << c << std::endl;
+	}
 
 	return 0;
 }
