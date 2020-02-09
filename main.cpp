542f4168ce67dbfeb174eb33b0677266e6e12d0d
Pedro Henrique Fernandes Lobo <lobophf@gmail.com>
2020-02-09 01:34:37 -0300

    I'm uploading the task of lecture S8L40

--- main.cpp
+++ main.cpp
@@ -1,29 +1,40 @@
 #include<iostream>
-#include<vector>
+#include<typeinfo>
+
+auto test() -> int{
+
+	return 8;
+}
+
+template<class T> 
+auto test1(T value) -> decltype(value){	
+	return value;
+}
+
+template<class T, class S>
+auto test2(T value1, S value2) -> decltype(value1 + value2){
+	return value1 + value2;
+}
+
+int get(){
+	return 99;
+}
+
+auto test3() -> decltype(get()){
+	return get();
+}
 
 int main(){
-	
-	auto texts = {"one", "two", "three"};
-	for(auto i: texts){
-		std::cout << i << std::endl;
-	}
-		
-	std::vector<int> numbers;
-	numbers.push_back(3);
-	numbers.push_back(5);
-	numbers.push_back(6);
-	numbers.push_back(11);
 
-	for(auto i: numbers){
-		std::cout << i << std::endl;
-	}
+	auto value = 9;
+	std::cout << value << std::endl;
 
-	std::string myString = "hello";
-	
-	std::cout << std::endl;
-	for(auto c: myString){
-		std::cout << c << std::endl;
-	}
+	std::cout << test1(87) << std::endl;
+	std::cout << test2(1, 6) << std::endl;
+
+	std::cout << typeid(test2(7, 4)).name() << std::endl;
+
+	std::cout << test3() << std::endl;
 
 	return 0;
 }
