7db98de57e31f2152aa373672d8b171a7f52a801
Pedro Henrique Fernandes Lobo <lobophf@gmail.com>
2020-02-10 17:31:34 -0300

    I'm uploading the task of lesson S8L44

--- main.cpp
+++ main.cpp
@@ -1,18 +1,26 @@
-#include<iostream>
+#include <iostream>
+#include <string>
 #include "ring.h"
 
 int main(){
 
-	Ring<std::string> r(3);
-
-	r.add("one");
-	r.add("two");
-	r.add("tree");
-	r.add("four");
-
-	for(int i = 0; i < r.size(); i++){
-		std::cout << r.get(i) << std::endl;
+	ring<std::string> textring(3);
+	
+	textring.add("one");
+	textring.add("two");
+	textring.add("three");
+	textring.add("four");
+      
+      	//C++ 98 style
+	for(ring<std::string>::iterator it = textring.begin(); it != textring.end(); it++){
+		std::cout << *it << std::endl;
 	}
 
+	std::cout << std::endl;
+
+      	// C++11 style
+	for(auto val : textring){
+		std::cout << val << std::endl;
+	}
 	return 0;
 }
--- ring.h
+++ ring.h
@@ -1,55 +1,95 @@
 #ifndef RING_H
 #define RING_H
-
 #include<iostream>
 
-template<typename T>
-class Ring{
+template<class T>
+class ring{
+	
 private:
-	T *m_ptr;
+	int m_pos;
 	int m_size;
-	int m_pos= 0;
+	T *m_values;
 
 public:
-	Ring(int size);
-	~Ring();
+	class iterator;
+public:
+	ring(int size);
+	~ring();
 	int size();
 	void add(T value);
 	T &get(int pos);
-};
 
-template<typename T>
-Ring<T>::Ring(int size){
-	m_size = size;
-	m_ptr = NULL;
-	m_ptr = new T[m_size];
-	m_pos = 0;
-};
+	iterator begin(){
+		return iterator(0, *this);
+	}
 
-template<typename T>
-void Ring<T>::add(T value){
-	m_ptr[m_pos] = value;
-	m_pos++;
-
-	if(m_pos == m_size){
-		m_pos = 0;
+	iterator end(){
+		return iterator(m_size, *this);
 	}
 };
 
 template<typename T>
-T& Ring<T>::get(int pos){
-	return m_ptr[pos]; 
-};
-
+ring<T>::ring(int size){ 
+	m_pos = 0;
+	m_size = size;
+	m_values = nullptr;
+	m_values = new T[size];
+}
 
 template<typename T>
-int Ring<T>::size(){
+ring<T>::~ring(){
+	delete[] m_values;
+}
+template<typename T>
+int ring<T>::size(){
 	return m_size;
-};
+}
 
 template<typename T>
-Ring<T>::~Ring(){
-	delete [] m_ptr;	
+void ring<T>::add(T value){
+	m_values[m_pos++] = value;
+	if(m_pos == m_size){
+		m_pos = 0;
+	}
+}
+
+template<typename T>
+T &ring<T>::get(int pos){
+	return m_values[pos];
+}
+
+template<class T>
+class ring<T>::iterator{
+private:
+	int m_pos;
+	ring &m_ring;
+public:
+	iterator(int pos, ring &ring_ref): m_ring(ring_ref){
+		m_pos = pos;
+	}
+
+	iterator operator++(int){
+		iterator old = *this;
+		++(*this);
+		return old;
+	}
+	
+	iterator& operator++(){
+		++m_pos;
+		return *this;
+	}
+
+	T& operator*(){
+		return m_ring.get(m_pos);
+	}
+
+	bool operator==(const iterator &other) const{
+		return m_pos == other.m_pos;
+	}
+
+	bool operator!=(const iterator &other) const{
+		return !(*this == other);	
+	}
 };
 
 #endif
