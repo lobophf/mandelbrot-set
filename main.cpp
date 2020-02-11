15a8131892f449b3ae033480bd642fa638b6268b
Pedro Henrique Fernandes Lobo <lobophf@gmail.com>
2020-02-11 17:23:29 -0300

    I'm uploading the S8L47 lecture

--- main.cpp
+++ main.cpp
@@ -1,45 +1,25 @@
 #include <iostream>
-#include <vector>
+#include <initializer_list>
 
-//c++11
-int main(){
-	
-	int value{4};
-	std::cout << value << std::endl;
-
-	std::string text{"Hello"};
-	std::cout << text << std::endl;
-	
-	int numbers[]{1, 2, 3};
-	std::cout << numbers[1] << std::endl;
-	
-	int *pInts = new int[3]{1, 2, 3};
-	std::cout << pInts[1] << std::endl;
-	delete pInts;
-
-	int values{};
-	std::cout << values << std::endl;
-
-	int *pSomething{nullptr};
-	std::cout << pSomething << std::endl;
-	
-	int numbers1[5]{};
-	std::cout << numbers[1] << std::endl;
-
-	struct{
-		int value;
-		std::string text;
-	} s1{4, "lalala"};
-
-	std::cout << s1.text << std::endl;	
-
-	std::vector<std::string> myVector {"one", "two", "three"};
-	std::cout << std::endl;
-
-	std::vector<std::string>::iterator it;
-	for(it = myVector.begin(); it != myVector.end(); it++){
-		std::cout << *it << std::endl;
+class Test{
+public:
+	Test(std::initializer_list<std::string> texts){
+		for(auto value: texts){
+			std::cout << value << std::endl;
+		}
 	}
+
+	void print(std::initializer_list<std::string> texts){
+		for(auto value: texts){
+			std::cout << value << std::endl;
+		}
+	}
+};
+
+int main(){
+
+	Test test{"apple", "orange", "banana"};
+	test.print({"one", "two", "three", "four"});
 
 	return 0;
 }
