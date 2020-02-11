b5baccc379a3e3296ab2c2aac90837d98cb8edb2
Pedro Henrique Fernandes Lobo <lobophf@gmail.com>
2020-02-11 14:12:56 -0300

    I'm uploading the lesson of S8L46

--- main.cpp
+++ main.cpp
@@ -1,36 +1,45 @@
 #include <iostream>
+#include <vector>
 
+//c++11
 int main(){
 	
-	int value[] = {1, 2, 3, 4};
+	int value{4};
+	std::cout << value << std::endl;
 
-	std::cout << value[3] << std::endl;
+	std::string text{"Hello"};
+	std::cout << text << std::endl;
+	
+	int numbers[]{1, 2, 3};
+	std::cout << numbers[1] << std::endl;
+	
+	int *pInts = new int[3]{1, 2, 3};
+	std::cout << pInts[1] << std::endl;
+	delete pInts;
 
-	class C{
-	public:
+	int values{};
+	std::cout << values << std::endl;
+
+	int *pSomething{nullptr};
+	std::cout << pSomething << std::endl;
+	
+	int numbers1[5]{};
+	std::cout << numbers[1] << std::endl;
+
+	struct{
+		int value;
 		std::string text;
-		int id;
-	};
-
-	C c1 = {"Hello", 3};
-
-	std::cout << c1.text << std::endl;	
-
-	struct S{
-		std::string text;
-		int id;
-	};
-
-	S s1 = {"lalala", 1};
+	} s1{4, "lalala"};
 
 	std::cout << s1.text << std::endl;	
 
-	struct {
-		std::string text;
-		int id;
-	} r1 = {"aaaaaaaaaaa", 5}, r2 = {"bbbbbbbbbbbb" , 4};
+	std::vector<std::string> myVector {"one", "two", "three"};
+	std::cout << std::endl;
 
+	std::vector<std::string>::iterator it;
+	for(it = myVector.begin(); it != myVector.end(); it++){
+		std::cout << *it << std::endl;
+	}
 
-	std::cout << r1.text << std::endl;	
 	return 0;
 }
