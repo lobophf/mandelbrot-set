a90138b6877c7f50bd68a6e1afad10a8762b0a97
Pedro Henrique Fernandes Lobo <lobophf@gmail.com>
2020-02-11 11:40:29 -0300

    I'm uploading the task of lesson S8L45

--- main.cpp
+++ main.cpp
@@ -1,26 +1,36 @@
 #include <iostream>
-#include <string>
-#include "ring.h"
 
 int main(){
-
-	ring<std::string> textring(3);
 	
-	textring.add("one");
-	textring.add("two");
-	textring.add("three");
-	textring.add("four");
-      
-      	//C++ 98 style
-	for(ring<std::string>::iterator it = textring.begin(); it != textring.end(); it++){
-		std::cout << *it << std::endl;
-	}
+	int value[] = {1, 2, 3, 4};
 
-	std::cout << std::endl;
+	std::cout << value[3] << std::endl;
 
-      	// C++11 style
-	for(auto val : textring){
-		std::cout << val << std::endl;
-	}
+	class C{
+	public:
+		std::string text;
+		int id;
+	};
+
+	C c1 = {"Hello", 3};
+
+	std::cout << c1.text << std::endl;	
+
+	struct S{
+		std::string text;
+		int id;
+	};
+
+	S s1 = {"lalala", 1};
+
+	std::cout << s1.text << std::endl;	
+
+	struct {
+		std::string text;
+		int id;
+	} r1 = {"aaaaaaaaaaa", 5}, r2 = {"bbbbbbbbbbbb" , 4};
+
+
+	std::cout << r1.text << std::endl;	
 	return 0;
 }
