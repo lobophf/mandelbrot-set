a42aa7653b4430893ac6a1720e2cc6b24dea0ca9
Pedro Henrique Fernandes Lobo <lobophf@gmail.com>
2020-02-13 16:36:22 -0300

    uploading the task of lesson S8L54

--- main.cpp
+++ main.cpp
@@ -1,47 +1,16 @@
 #include <iostream>
-#include <functional> 
-#include <vector>
-#include <algorithm>
 
-bool check(std::string &test){
-	return test.size() == 3;
-}
-
-class Check{
-public:
-	bool operator()(std::string &test){
-		return test.size() == 5;
-	}
-} check1;
-
-void run(std::function<bool(std::string&)> check){
-	std::string test = "dog";
-	std::cout << check(test) << std::endl;
-}
+#include<iostream>
 
 int main(){
 
-	int size = 5;
+	int cats = 5;
+	[cats]() mutable{
+		cats = 9;
+		std::cout << cats << std::endl;	
+	}();
 
-	std::vector<std::string> vec{"one", "two", "three"};
-	int count = count_if(vec.begin(), vec.end(),
-				[size](std::string test){return test.size() == size;});
-	std::cout << count << std::endl;
-
-	auto lambda = [size](std::string test){return test.size() == size;};
-	count = count_if(vec.begin(), vec.end(), lambda);
-
-	std::cout << count << std::endl;
-
-	count = count_if(vec.begin(), vec.end(), check1);
-	std::cout << count << std::endl;
-
-	run(lambda);
-	run(check1);
-	run(check);
-
-	std::function<int(int, int)> add = [](int one, int two){return one + two;};
-	std::cout << add(3, 8) << std::endl;
+	std::cout << cats << std::endl;
 
 	return 0;
 }
