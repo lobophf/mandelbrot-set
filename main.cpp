3f8ad4bb69d19aa2cba794c91bf5176f9c8ca110
Pedro Henrique Fernandes Lobo <lobophf@gmail.com>
2020-02-12 21:33:57 -0300

    I'm uploading the task of S8L49

--- main.cpp
+++ main.cpp
@@ -1,32 +1,16 @@
 #include <iostream>
 
-class Test{
-private:
-	int id{2};
-	std::string name{"Mike"};
-public:
-	Test() = default;
-//	Test(const Test &other) = delete;
-	Test &operator=(const Test &other) = default;
-	Test(int id) : id(id){
-
-	}
-
-	void print(){
-		std::cout << id << ": " << name << std::endl;
-	}
-};
+void test(void (*pFunc)()){
+	pFunc();
+}
 
 int main(){
 
-	Test test;
-	test.print();
-
-	Test test1(4);
-	test1.print();
-
-//	Test test2 = test1;
-
+	auto func = [](){std::cout << "Hello" << std::endl;};
+	func();
+	
+	test(func);
+	test([](){std::cout << "Hello" << std::endl;});
 
 	return 0;
 }
