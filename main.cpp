40a1bf2f768ce2a0d09c48d9672edec335807bb4
Pedro Henrique Fernandes Lobo <lobophf@gmail.com>
2020-02-13 17:06:17 -0300

    Uploading the task of lesson S8L55

--- main.cpp
+++ main.cpp
@@ -1,16 +1,27 @@
-#include <iostream>
-
 #include<iostream>
+
+class Parent{
+	int dogs{5};
+	std::string text{"hello"};
+public:
+	Parent() : Parent("hello"){
+		std::cout << "no parameter parent construtor" << std::endl;
+	}
+
+	Parent(std::string text){
+		std::cout << "string parent constructor" << std::endl;
+	}
+};
+
+class Child: public Parent{
+public:
+	Child() = default;
+};
 
 int main(){
 
-	int cats = 5;
-	[cats]() mutable{
-		cats = 9;
-		std::cout << cats << std::endl;	
-	}();
-
-	std::cout << cats << std::endl;
+	Parent parent("hello");
+	Child child;
 
 	return 0;
 }
