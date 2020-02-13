67928237b8511ec509ea68d9f4c8a5ab49acdec1
Pedro Henrique Fernandes Lobo <lobophf@gmail.com>
2020-02-13 16:21:51 -0300

    Uploading the lesson S8L53

--- main.cpp
+++ main.cpp
@@ -1,31 +1,47 @@
 #include <iostream>
+#include <functional> 
+#include <vector>
+#include <algorithm>
 
-class Test{
-private:
-	int one{1};
-	int two{2};
+bool check(std::string &test){
+	return test.size() == 3;
+}
 
-
+class Check{
 public:
-	void run(){
-		int three{3};
-		int four{4};
-
-		auto pLambda = [this, three, four](){
-			one = 111;
-			std::cout << one << std::endl;
-			std::cout << two << std::endl;
-			std::cout << three << std::endl;
-			std::cout << four << std::endl;
-		};
-		pLambda();
+	bool operator()(std::string &test){
+		return test.size() == 5;
 	}
-};
+} check1;
+
+void run(std::function<bool(std::string&)> check){
+	std::string test = "dog";
+	std::cout << check(test) << std::endl;
+}
 
 int main(){
 
-	Test test;
-	test.run();
+	int size = 5;
+
+	std::vector<std::string> vec{"one", "two", "three"};
+	int count = count_if(vec.begin(), vec.end(),
+				[size](std::string test){return test.size() == size;});
+	std::cout << count << std::endl;
+
+	auto lambda = [size](std::string test){return test.size() == size;};
+	count = count_if(vec.begin(), vec.end(), lambda);
+
+	std::cout << count << std::endl;
+
+	count = count_if(vec.begin(), vec.end(), check1);
+	std::cout << count << std::endl;
+
+	run(lambda);
+	run(check1);
+	run(check);
+
+	std::function<int(int, int)> add = [](int one, int two){return one + two;};
+	std::cout << add(3, 8) << std::endl;
 
 	return 0;
 }
