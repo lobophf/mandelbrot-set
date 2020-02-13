b3fec99fb8fab52a4a74ce2e9a82ba21c6cb8fac
Pedro Henrique Fernandes Lobo <lobophf@gmail.com>
2020-02-12 22:19:20 -0300

    uploading the S8L50

--- main.cpp
+++ main.cpp
@@ -1,16 +1,34 @@
 #include <iostream>
 
-void test(void (*pFunc)()){
-	pFunc();
+void testGreet(void (*greet)(std::string)){
+	greet("Bob");
+}
+
+void runDivide(double (*divide)(double a, double b)){
+	double rval = divide(9, 2);
+	std::cout << rval << std::endl;
 }
 
 int main(){
 
-	auto func = [](){std::cout << "Hello" << std::endl;};
-	func();
+	auto pGreet = [](std::string name){std::cout << "hello " << std::endl;};
+	pGreet("Mike");
+
+	testGreet(pGreet);
+
+	auto pDivide = [](double a, double b) -> double{
+
+		if(b == 0){
+			return 0;
+		}
+		return a/b;
+	};
+
+	std::cout << pDivide(10.0, 4.0) << std::endl;
+	std::cout << pDivide(10.0, 0.0) << std::endl;
 	
-	test(func);
-	test([](){std::cout << "Hello" << std::endl;});
+	runDivide(pDivide);
+
 
 	return 0;
 }
