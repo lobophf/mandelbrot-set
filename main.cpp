bf55f83b32335fa3f1afb16017e6a73fab1d37e6
Pedro Henrique Fernandes Lobo <lobophf@gmail.com>
2020-02-15 13:06:57 -0300

    Uploading the lesson of S8L57

--- main.cpp
+++ main.cpp
@@ -1,26 +1,42 @@
 #include <iostream>
+#include <memory.h>
 #include <vector>
 
 class Test{
+private:
+	static const int SIZE=100;
+	int *_pBuffer;
+
 public:
 	Test(){
 		std::cout << "constructor" << std::endl;
+		_pBuffer = new int[SIZE];
+		//_pBuffer = new int[SIZE](); C++11;
+		memset(_pBuffer, 0, sizeof(int)*SIZE);
 	}
 
 	Test(int i){
 		std::cout << "parametrized constructor" << std::endl;
+
+		for(int i = 0; i < SIZE; i++){
+			_pBuffer[i] = 7 * i;
+		}
 	}
 	
 	Test(const Test &other){
 		std::cout << "copy constructor" << std::endl;
+//		memcpy(_pBuffer, other._pBuffer, SIZE*sizeof(int));
 	}
 
 	Test &operator=(const Test &other){
 		std::cout << "assigment" << std::endl;
+//		memcpy(_pBuffer, other._pBuffer, SIZE*sizeof(int));
+		return *this;
 	}
 	
 	~Test(){
 		std::cout << "destructor" << std::endl;
+		delete [] _pBuffer;
 	}
 
 	friend std::ostream &operator<<(std::ostream &out, const Test &test);
@@ -46,7 +62,6 @@ int main(){
 
 	std::vector<Test> vec;
 	vec.push_back(Test());
-
 
 	return 0;
 }
