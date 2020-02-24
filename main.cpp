8bb469c1cc6084abc84a05d38eb5368f60d48916
Pedro Henrique Fernandes Lobo <lobophf@gmail.com>
2020-02-24 15:36:34 -0300

    I'm uploading the lesson of S8L61

--- main.cpp
+++ main.cpp
@@ -5,18 +5,16 @@
 class Test{
 private:
 	static const int SIZE=100;
-	int *_pBuffer;
+	int *_pBuffer{nullptr};
 
 public:
 	Test(){
-		std::cout << "constructor" << std::endl;
 		_pBuffer = new int[SIZE];
 		//_pBuffer = new int[SIZE](); C++11;
 		memset(_pBuffer, 0, sizeof(int)*SIZE);
 	}
 
 	Test(int i){
-		std::cout << "parametrized constructor" << std::endl;
 
 //		for(int i = 0; i < SIZE; i++){
 //			_pBuffer[i] = 7 * i;
@@ -24,39 +22,34 @@ public:
 	}
 	
 	Test(const Test &other){
-		std::cout << "copy constructor" << std::endl;
 //		memcpy(_pBuffer, other._pBuffer, SIZE*sizeof(int));
 	}
 
 	Test &operator=(const Test &other){
-		std::cout << "assigment" << std::endl;
 //		memcpy(_pBuffer, other._pBuffer, SIZE*sizeof(int));
 		return *this;
 	}
 	
 	~Test(){
-		std::cout << "destructor" << std::endl;
 		delete [] _pBuffer;
 	}
 
 	friend std::ostream &operator<<(std::ostream &out, const Test &test);
-};
 
+	Test(Test &&other){
+	
+		std::cout << "move constructor" << std::endl;
+		_pBuffer = other._pBuffer;
+		other._pBuffer = nullptr;
+	}
+
+};
 std::ostream &operator<<(std::ostream &out, const Test &test){
-	out << "Hello from test" << std::endl;
 	return out;
 }
 
 Test getTest(){
 	return Test();
-}
-
-void check(const Test &value){
-	std::cout << "lValue function!" << std::endl;
-}
-
-void check(Test &&value){
-	std::cout << "rValue function!" << std::endl;
 }
 
 int main(){
@@ -66,13 +59,8 @@ int main(){
 	std::vector<Test> vec;
 	vec.push_back(Test());
 
-	Test &ltest1 = test1;
 
-	Test &&rtest1 = getTest();
-	
-	check(test1);
-	check(getTest());
-	check(Test());
+
 
 	return 0;
 }
