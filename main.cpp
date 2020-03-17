5d2be8255910282c779b88b7c73b19a553f0aff8
Pedro Henrique Fernandes Lobo <lobophf@gmail.com>
2020-03-17 14:45:28 -0300

    feat: I added a color histogram
    The instructor shows you how to make a monochrome color gradient based
    on a histogram. This histogram is a normalization of the number of
    iterations that passes through each pixel.

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include "includes/mandelbrot.h"
 #include <iostream>
 #include <cstdint>
+#include <memory>
+#include <math.h>
 
 int main(){
 
@@ -10,25 +12,46 @@ int main(){
 
 	bmp::Bitmap bm(WIDTH, HEIGHT);
 
-	double min = 9999999;
-	double max = -9999999;
+	std::unique_ptr<int[]> histogram(new int[Mandelbrot::MAX_ITERATIONS]{0});
+	std::unique_ptr<int[]> fractal(new int[WIDTH * HEIGHT]{0});
 
 	for(int y = 0; y < HEIGHT; y++){
 		for(int x = 0; x < WIDTH; x++){
 			double xFractal = (x - WIDTH / 2.0 - 200) * 2.0 / HEIGHT;
 			double yFractal = (y - HEIGHT / 2.0) * 2.0 / HEIGHT;
-			
 			int iterations = Mandelbrot::getIterations(xFractal, yFractal);
-			uint8_t red = (uint8_t)(256 * (double)iterations/Mandelbrot::MAX_ITERATIONS);
-			
-			bm.setPixel(x, y, red, 0, 0);
-
-			if(red < min) min = red;
-			if(red > max) max = red;
+			fractal[y * WIDTH + x] = iterations;
+			if(iterations != Mandelbrot::MAX_ITERATIONS){
+				histogram[iterations]++;
+			}
 		}
 	}
+	
+	int total = 0;
+	for(int i = 0; i < Mandelbrot::MAX_ITERATIONS; i++){
+		total += histogram[i];
+	}
 
-	std::cout << min << ", " << max << std::endl;
+	for(int y = 0; y < HEIGHT; y++){
+		for(int x = 0; x < WIDTH; x++){
+
+			uint8_t red = 0;
+			uint8_t green = 0;
+			uint8_t blue = 0;
+
+			int iterations = fractal[y * WIDTH + x];
+			double hue = 0.0;
+			for(int i = 0; i <= iterations; i++){
+				hue += (double)histogram[i]/total;
+			}
+		
+			if(iterations != Mandelbrot::MAX_ITERATIONS){
+				green = pow(255, hue);
+			}
+
+			bm.setPixel(x, y, red, green, blue);
+		}
+	}
 
 	bm.write("outputs/test.bmp");
 	std::cout << "Finished." << std::endl;
