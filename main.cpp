417a3961861d3380f51b91cd286612daba64831b
Pedro Henrique Fernandes Lobo <lobophf@gmail.com>
2020-03-16 23:34:19 -0300

    feat: Implementing the scaling algorithm
    The instructor showed a way to implement the scale factor ranging from
    -1 to 1. I didn't understand everything, but I'm following him anyway.

--- main.cpp
+++ main.cpp
@@ -7,6 +7,22 @@ int main(){
 	int const height = 600;
 
 	bmp::Bitmap bm(width, height);
+
+	double min = 9999999;
+	double max = -9999999;
+
+	for(int y = 0; y < height; y++){
+		for(int x = 0; x < width; x++){
+			double xFractal = (x - width / 2.0) * 2.0 / width;
+			double yFractal = (y - height/ 2.0) * 2.0 / height;
+
+			if(xFractal < min) min = xFractal;
+			if(xFractal > max) max = xFractal;
+		}
+	}
+
+	std::cout << min << ", " << max << std::endl;
+
 	bm.setPixel(width / 2, height / 2, 255, 255, 255);
 	bm.write("outputs/test.bmp");
 
